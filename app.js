8d67991c57d056053a5068e3dddce73ef1029c65
root <kailiao2000@gmail.com>
2019-10-29 03:35:47 +0000

    add initial routes

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
 	var campgrounds = [
-		{name: "guangzhou tower", image:"https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_720,f_auto/w_80,x_15,y_15,g_south_west,l_klook_water/activities/jeoolpih7jpkduacsvno/.jpg"},
-		{name: "baiyun mountain", image: ""},
-		{name: "the pearl river", image: ""}
+		{name: "Canton Tower", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Canton_Tower_2013.11.15_18-12-45.jpg/225px-Canton_Tower_2013.11.15_18-12-45.jpg"},
+		{name: "baiyun mountain", image: "http://p4-q.mafengwo.net/s10/M00/69/83/wKgBZ1nXdhuAYxjAAAu_dfzu4oI78.jpeg?imageMogr2%2Fthumbnail%2F%21305x183r%2Fgravity%2FCenter%2Fcrop%2F%21305x183%2Fquality%2F100"},
+		{name: "the pearl river", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Pearl_River_Canton_Tower.JPG/220px-Pearl_River_Canton_Tower.JPG"}
 	]
 	res.render("campgrounds", {campgrounds:campgrounds});
 });
