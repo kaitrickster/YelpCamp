b462a6906ee4bdb98547e91ce06ed8d6d294e271
root <kailiao2000@gmail.com>
2019-10-30 07:15:25 +0000

    add mongod

--- app.js
+++ app.js
@@ -1,36 +1,68 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-		{name: "Canton Tower", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Canton_Tower_2013.11.15_18-12-45.jpg/225px-			Canton_Tower_2013.11.15_18-12-45.jpg"},
-		{name: "baiyun mountain", image: "http://p4-q.mafengwo.net/s10/M00/69/83/wKgBZ1nXdhuAYxjAAAu_dfzu4oI78.jpeg?							imageMogr2%2Fthumbnail%2F%21305x183r%2Fgravity%2FCenter%2Fcrop%2F%21305x183%2Fquality%2F100"},
-		{name: "the pearl river", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Pearl_River_Canton_Tower.JPG/220px-			Pearl_River_Canton_Tower.JPG"}
-];
-
-app.get("/", function(req, res){
-	res.render("landing");
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema ({
+    name: String,
+    image: String,
+    description: String
 });
 
-app.get("/campgrounds", function(req, res){
-	res.render("campgrounds", {campgrounds:campgrounds});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+       
+app.get("/", function(req, res) {
+    res.render("landing");
 });
 
-
-app.post("/campgrounds", function(req, res) {
-	var name = req.body.name;
-	var image = req.body.image;
-	var newCampground = {name:name, image:image}
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+//INDEX ROUTE - show all campgrounds
+app.get("/campgrounds", function(req, res) {
+    Campground.find({}, function(err, allCampgrounds){
+        if (err) {
+            console.log(err);
+        } else {
+             res.render("index", {campgrounds:allCampgrounds}); 
+        }   
+        });
+    
 });
 
+//CREATE - add new campgrounds to database
+app.post("/campgrounds", function (req, res){
+    var name= req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+   Campground.create(newCampground, function(err, newlyCreated){
+      if (err) {
+          console.log(err);
+      } else {
+          res.redirect("/campgrounds"); //
+      }
+   });
+});
 
+//NEW - show form to create new campground 
 app.get("/campgrounds/new", function(req, res){
-	res.render("new.ejs");
+   res.render("new.ejs") 
+});
+
+//SHOW - shows more info about campground selected - to be declared after NEW to not overwrite
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+       if (err) {
+           console.log(err);
+       } else {
+           res.render("show", {campground: foundCampground});
+       }
+    });
 });
 
 
